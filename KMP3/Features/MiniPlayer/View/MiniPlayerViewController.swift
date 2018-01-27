bccd45a275446fac8abd2b478babaf9173aa3376
Khuong Pham <dkhuong291@gmail.com>
2018-01-27 19:26:11 +0700

    Add MiniPlayerViewController

---
+++ KMP3/Features/MiniPlayer/View/MiniPlayerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MiniPlayerViewController.swift
+//  KMP3
+//
+//  Created by KhuongPham on 1/27/18.
+//
+
+import Foundation
+import UIKit
+
+final class MiniPlayerViewController: UIViewController {
+    
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var settingButton: UIButton!
+    @IBOutlet weak var songNameLabel: UILabel!
+    @IBOutlet weak var authorNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func playPauseButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func settingButtonTapped(_ sender: UIButton) {
+        
+    }
+}
