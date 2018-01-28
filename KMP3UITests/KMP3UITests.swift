ce0ff094fff423b344739792a1c7dffcfa62f57e
khuong291 <dkhuong291@gmail.com>
2018-01-28 17:21:11 +0700

    Write some UI Tests

--- KMP3UITests/KMP3UITests.swift
+++ KMP3UITests/KMP3UITests.swift
@@ -8,28 +8,36 @@
 import XCTest
 
 class KMP3UITests: XCTestCase {
-        
+    var app: XCUIApplication!
+    
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
+        app = XCUIApplication()
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testScrolling() {
+        app.launch()
+        
+        let tableView = app.tables.element(boundBy: 0)
+        tableView.swipeUp()
+        tableView.swipeUp()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPlayAndPause() {
+        app.launch()
+        
+        let firstCell = app.cells.element(boundBy: 0)
+        let playButton = firstCell.buttons["ic play rounded"]
+        playButton.tap()
+        
+        sleep(3)
+        
+        let pauseButton = firstCell.buttons["ic pause rounded"]
+        pauseButton.tap()
+        
+        sleep(2)
     }
     
 }
